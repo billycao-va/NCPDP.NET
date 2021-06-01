019a9034e980c3c58f4d36de302fa34d5b7e9c59
billycao-va <68917921+billycao-va@users.noreply.github.com>
2021-06-01 08:01:24 -0700

    Update SerialKey.cs

--- EdiFabric.Examples.NCPDP.Telco.Common/SerialKey.cs
+++ EdiFabric.Examples.NCPDP.Telco.Common/SerialKey.cs
@@ -10,8 +10,12 @@ namespace EdiFabric.Examples.NCPDP.Telco.Common
         {
             var serialKeyPath = @"../../../../edifabric-trial/serial.key";
 
+            var fullPath = Path.GetFullPath(serialKeyPath);
+            Console.WriteLine("GetFullPath('{0}') returns '{1}'",
+                serialKeyPath, fullPath);
+
             if (!File.Exists(serialKeyPath))
-                throw new Exception("Set the path to the serial.key file in project EdiFabric.Examples.NCPDP.Telco.Common, file SerialKey.cs!");
+                throw new Exception("Set the path to the serial.key file in project EdiFabric.Examples.NCPDP.Telco.Common, file SerialKey.cs! " + fullPath);
 
             _serialKey = File.ReadAllText(serialKeyPath).Trim(new[] { ' ', '\r', '\n' });
         }
